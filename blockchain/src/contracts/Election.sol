f816bab7a3b609de66c4a577b81d2be94182771a
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2022-04-19 20:17:00 +0530

    local to git

---
+++ blockchain/src/contracts/Election.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Election {
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+        string details;
+        string election_id;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
+
+    uint public candidatesCount;
+
+    string public candidate;
+
+    constructor() {}
+
+    event votedEvent(
+        uint indexed _candidateId
+    );
+
+    function addCandidate(string memory _name, string memory _details, string memory _election_id) public {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0, _details, _election_id);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender]);
+
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        
+        voters[msg.sender] = true;
+        
+        candidates[_candidateId].voteCount++;
+    
+        emit votedEvent(_candidateId);
+    }
+
+}
